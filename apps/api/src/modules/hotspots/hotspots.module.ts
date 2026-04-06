50e6f50115539fd65a823a6384d8d520324809bd
Benjamin Angella <90790158+UBenTech@users.noreply.github.com>
2026-04-06 10:19:20 +0300

    Fix: Add @nestjs/config dep and correct relative prisma.service import paths

--- apps/api/src/modules/hotspots/hotspots.module.ts
+++ apps/api/src/modules/hotspots/hotspots.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../../prisma.service';
 
 export class HotspotsService {
   constructor(private prisma: PrismaService) {}
