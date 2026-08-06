f9c794e6153dd043ac46750ac6907c6417a5dcdd
AROSOFT Innovations Ltd <arosoftio@gmail.com>
2026-08-06 22:47:12 +0300

    fix(vouchers): enable public QR routing policy

--- apps/api/src/modules/vouchers/vouchers.module.ts
+++ apps/api/src/modules/vouchers/vouchers.module.ts
@@ -5,13 +5,14 @@ import { MailModule } from '../mail/mail.module'
 import { PaymentsModule } from '../payments/payments.module'
 import { WhatsAppModule } from '../whatsapp/whatsapp.module'
 import { VoucherCodeService } from './voucher-code.service'
+import { VoucherQrRoutingInitializer } from './voucher-qr-routing.initializer'
 import { VouchersController } from './vouchers.controller'
 import { VouchersService } from './vouchers.service'
 
 @Module({
   imports: [AuthModule, BillingModule, MailModule, PaymentsModule, WhatsAppModule],
   controllers: [VouchersController],
-  providers: [VoucherCodeService, VouchersService],
+  providers: [VoucherCodeService, VouchersService, VoucherQrRoutingInitializer],
   exports: [VouchersService],
 })
 export class VouchersModule {}
