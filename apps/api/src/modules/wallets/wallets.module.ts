6d155788f5d9374005d704bff6f8f1dd0e28673a
AROSOFT Innovations Ltd <arosoftio@gmail.com>
2026-08-14 19:09:23 +0300

    Wire atomic wallet topup reconciler

--- apps/api/src/modules/wallets/wallets.module.ts
+++ apps/api/src/modules/wallets/wallets.module.ts
@@ -3,13 +3,14 @@ import { AuthModule } from '../auth/auth.module'
 import { MailModule } from '../mail/mail.module'
 import { PaymentsModule } from '../payments/payments.module'
 import { IotecWalletWebhookController } from './iotec-wallet-webhook.controller'
+import { WalletTopupStatusService } from './wallet-topup-status.service'
 import { WalletsController } from './wallets.controller'
 import { WalletsService } from './wallets.service'
 
 @Module({
   imports: [AuthModule, MailModule, PaymentsModule],
   controllers: [WalletsController, IotecWalletWebhookController],
-  providers: [WalletsService],
+  providers: [WalletsService, WalletTopupStatusService],
   exports: [WalletsService],
 })
 export class WalletsModule {}
